84fca617880e3213d9aac85468b1a69b1f465793
nourhanaboelsoud <nora5234329@gmail.com>
2025-09-12 11:03:10 -0700

    Update my branch with correct files

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:final_project/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:final_project/views/login_page.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized(); 
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform); //initialize firebase in the app
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'SaveIt',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(), // تبدأ بصفحة تسجيل الدخول
+    );
+  }
+}
