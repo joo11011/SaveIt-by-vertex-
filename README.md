30819691d783dadddd62384e46825f33a08e8177
nourhanaboelsoud <nora5234329@gmail.com>
2025-09-12 10:54:28 -0700

    Remove old duplicated files

--- README.md
+++
@@ -1,3 +0,0 @@
-# final_project
-
-A new Flutter project.
--- android/app/src/main/kotlin/com/example/savelt_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.savelt_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
